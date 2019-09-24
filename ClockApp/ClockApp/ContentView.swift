303bc59f15ee43ee79aefa49e00acc0dda70f7dd
Taylah Lucas <taylahlucas@gmail.com>
2019-09-24 12:03:37 +1000

    Initial commit
    Initial project creation - no code has been written yet

---
+++ ClockApp/ClockApp/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  ClockApp
+//
+//  Created by Taylah Lucas on 24/9/19.
+//  Copyright © 2019 Taylah Lucas. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection = 0
+ 
+    var body: some View {
+        TabView(selection: $selection){
+            Text("First View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("first")
+                        Text("First")
+                    }
+                }
+                .tag(0)
+            Text("Second View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("second")
+                        Text("Second")
+                    }
+                }
+                .tag(1)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
