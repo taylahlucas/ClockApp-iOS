38c2ddd2b641c9a5f2fd858f1b4026bb932b1eee
Taylah Lucas <taylahlucas@gmail.com>
2019-10-02 09:18:47 +1000

    Added tab bar controller and stopwatch page

--- ClockApp/AppDelegate.swift
+++ ClockApp/AppDelegate.swift
@@ -19,7 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         window = UIWindow(frame: UIScreen.main.bounds)
-        navigationController = UINavigationController(rootViewController: ShowAlarmsViewController())
+        navigationController = UINavigationController(rootViewController: TabBarController())
         window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
 
---
+++ ClockApp/TabBarController.swift
@@ -0,0 +1,26 @@
+//
+//  TabBarController.swift
+//  ClockApp
+//
+//  Created by Taylah Lucas on 2/10/19.
+//  Copyright © 2019 Taylah Lucas. All rights reserved.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+         
+        let showAlarmsViewcontroller = ShowAlarmsViewController()
+        showAlarmsViewcontroller.tabBarItem = UITabBarItem(title: "Alarms", image: .none, tag: 0)
+        
+        let stopwatchViewController = StopwatchViewController()
+        stopwatchViewController.tabBarItem = UITabBarItem(title: "Stopwatch", image: .none, tag: 1)
+        
+        let tabBarList = [showAlarmsViewcontroller, stopwatchViewController]
+        
+        viewControllers = tabBarList
+    }
+}
